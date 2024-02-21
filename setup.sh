567a817c10f26c382a0b7367f2074b2cc4c6fa7a
xi zhang <chaucyzhang@gmail.com>
2024-02-21 20:10:58 +0800

    update setup sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yes Y | sudo apt update;
+yes Y | sudo apt install shadowsocks-libev;
+nohup ss-manager -c /etc/shadowsocks-libev/ss.json -u manager.json &;
+sudo systemctl restart shadowsocks-libev;
+sudo systemctl status shadowsocks-libev;
+SYSCTL_CONF=/etc/sysctl.d/60-tcp-bbr.conf;
+echo "net.core.default_qdisc=fq" | sudo tee $SYSCTL_CONF;
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a $SYSCTL_CONF;
+sudo sysctl -p $SYSCTL_CONF;
+sysctl net.ipv4.tcp_available_congestion_control;
+sysctl net.ipv4.tcp_congestion_control;
+lsmod | grep bbr;
