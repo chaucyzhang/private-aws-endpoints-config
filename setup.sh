5e9750362d8f6bf30f4ea54ee587026471ad0f7f
xi zhang <chaucyzhang@gmail.com>
2024-02-21 21:16:30 +0800

    update script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yes Y | sudo apt update;
 yes Y | sudo apt install shadowsocks-libev;
-wget https://github.com/chaucyzhang/private-aws-endpoints-config/blob/main/default.json -P /etc/shadowsocks-libev
-nohup ss-manager -c /etc/shadowsocks-libev/default.json -u manager.json &;
+sudo wget https://gist.githubusercontent.com/chaucyzhang/8f0772e401e3e22c49e9d454ff85ce08/raw/1d9e7b7d6879b737e8558e556a8cd67aedffea58/default.json -P /etc/shadowsocks-libev;
+nohup ss-manager -c /etc/shadowsocks-libev/default.json -u manager.json &
 sudo systemctl restart shadowsocks-libev;
 sudo systemctl status shadowsocks-libev;
 SYSCTL_CONF=/etc/sysctl.d/60-tcp-bbr.conf;
